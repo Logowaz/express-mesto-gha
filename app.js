38bf0d29d5afaf26dd31c661e15713b4d8d15fdf
Igor Fedyanin <logowaz25@yandex.ru>
2024-01-21 00:37:22 +0300

    14 ПР на проверку4

--- app.js
+++ app.js
@@ -11,16 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect('mongodb://localhost:27017/mestodb');
 // mongoose.connect('mongodb://127.0.0.1/mestodb');
-
-app.use(cookieParser());
-app.use(router);
-app.use(errors());
-app.use(errorHandler);
-app.listen(PORT, () => {
-  console.log(`Слушаю порт ${PORT}`);
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
